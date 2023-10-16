3d95d244ecb0ba399633b3ba25b31066dd9c1aff
Anishka Kesaria <kescotak@gmail.com>
2023-10-17 01:53:40 +0530

    Backend added

---
+++ server/utils/mailSender.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+
+const mailSender = async (email, title, body) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                service:'gmail',
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: `"Study Notion" <${process.env.MAIL_USER}>`,
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+        return error;
+    }
+}
+
+
+module.exports = mailSender;
